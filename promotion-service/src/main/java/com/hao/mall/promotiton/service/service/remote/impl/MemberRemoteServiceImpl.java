498a505575d506041c795751d4e22680f17aaa71
lidihao <dihao.li@miniso.com>
2023-11-15 16:48:28 +0800

    促销系统

---
+++ promotion-service/src/main/java/com/hao/mall/promotiton/service/service/remote/impl/MemberRemoteServiceImpl.java
@@ -0,0 +1,18 @@
+/**
+ * @author lidihao
+ * @Description:
+ */
+
+package com.hao.mall.promotiton.service.service.remote.impl;
+
+import com.hao.mall.promotiton.service.dto.MemberDto;
+import com.hao.mall.promotiton.service.service.remote.MemberRemoteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberRemoteServiceImpl implements MemberRemoteService {
+    @Override
+    public MemberDto getMemberDtoById(Long memberId) {
+        return null;
+    }
+}
